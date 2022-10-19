bf74b0860d2121c2c969f9d5b9484f23d84fc7cd
Cintia Ruiz <cintiaruizv@gmail.com>
2022-10-18 19:55:26 -0500

    Finish exercise 01

--- exercises/01/src/index.js
+++ exercises/01/src/index.js
@@ -1,1 +1,9 @@
 // Create your root and call the render method with your App component. Don't forget to use StrictMode
+
+import React from 'react' //react es un ambiente separado de donde se quiere renderizar
+import ReactDom from 'react-dom/client' //queremos renderizarlo en la web
+import App from './App'
+
+const root = ReactDom.createRoot(document.getElementById('root'))
+
+root.render(<App />)//aqui ya puedo empezar a escibir JSX
