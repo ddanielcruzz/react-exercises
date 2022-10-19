4a478cbe061c680ea88617c3f9a2336f73239420
Rodrigo Montoya <montoyar32@gmail.com>
2022-10-18 19:55:25 -0500

    First exercise finished

--- exercises/01/src/index.js
+++ exercises/01/src/index.js
@@ -1,1 +1,10 @@
 // Create your root and call the render method with your App component. Don't forget to use StrictMode
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+//This is the starting point for the component
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//Now we need to render the component.
+root.render(<App/>)
