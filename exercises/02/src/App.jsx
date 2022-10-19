460f19838714e2abd1293c3c758fe70d1345910f
Daniel Cruz <nox.sb@hotmail.com>
2022-10-19 18:51:05 -0500

    Update starter code for exercises-02

--- exercises/02/src/App.jsx
+++ exercises/02/src/App.jsx
@@ -48,18 +48,21 @@ const products = [
 
 function App() {
   return (
-    <article className="card">
-      <p className="card__category">perfume</p>
-      <h1 className="card__title">Gabrielle Essence Eau De Parfum</h1>
-      <p className="card__text">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam
-        aspernatur autem nobis quidem at sed, incidunt necessitatibus aliquid.
-        Eligendi ipsam obcaecati dolores vel quasi eum laborum magnam libero.
-        Obcaecati, consequuntur.
-      </p>
-      <p className="card__price">$149.99</p>
-      <button className="card__button">Add to Cart</button>
-    </article>
+    <main>
+      {/* Only move to a component the code inside this main. Leave the main tag here */}
+      <article className="card">
+        <p className="card__category">perfume</p>
+        <h1 className="card__title">Gabrielle Essence Eau De Parfum</h1>
+        <p className="card__text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam
+          aspernatur autem nobis quidem at sed, incidunt necessitatibus aliquid.
+          Eligendi ipsam obcaecati dolores vel quasi eum laborum magnam libero.
+          Obcaecati, consequuntur.
+        </p>
+        <p className="card__price">$149.99</p>
+        <button className="card__button">Add to Cart</button>
+      </article>
+    </main>
   );
 }
 
