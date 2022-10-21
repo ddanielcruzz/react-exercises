946384202dd35b5cde26d33613cd29730873f685
Cintia Ruiz <cintiaruizv@gmail.com>
2022-10-20 19:06:42 -0500

    session 2

--- exercises/02/src/components/ProductCard/ProductCard.jsx
+++ exercises/02/src/components/ProductCard/ProductCard.jsx
@@ -4,19 +4,23 @@ import {BuyButton, SoldOutButton} from '../SoldOut/buyButton'
 
 
 function Card(props) {
-  
+    const { id, category, title, description, price, stock} = props; /* destructuring */
+
+    const handleClick = (event) => {
+        event.stopPropagation();
+        alert('le diste click al producto '+ title)
+    }
     return (
       <article className="card">
-        <p className="card__category">{props.category}</p>
-        <h1 className="card__title">{props.title}</h1>
-        <p className="card__text">{props.description}</p>
-        <p className="card__price">${props.price}</p>
+        <p className="card__category">{category}</p>
+        <h1 className="card__title">{title}</h1>
+        <p className="card__text">{description}</p>
+        <p className="card__price">${price}</p>
+        <button className='card__button' onClick={() => handleClick()}> click 
+        </button>
         <section>
          {props.stock === 0 ? <SoldOutButton/> : <BuyButton/>}
        </section> 
- {/*          {props.isSoldOut ? <SoldOutButton /> : <BuyButton />}
-  */}         {/* //si el art esta soldout pasale el componente soldoutbutton  */}
-        
       </article>
     );
     } 
